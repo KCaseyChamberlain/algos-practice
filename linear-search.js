d58dfae161ad167e2f0ae1f66f61cef9fc717c91
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-06-19 18:27:13 -0600

    linear algorithm added

---
+++ linear-search.js
@@ -0,0 +1,16 @@
+// Linear search!
+// This code looks through every number in the array to find the index that corresponds
+// to the input number.
+
+const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+const findIndex = (num) => {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] === num) {
+            console.log(`${num} found at index ${i}`);
+        }
+    }
+};
+
+// input
+findIndex(40)
