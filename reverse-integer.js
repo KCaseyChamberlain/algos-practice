e224cb7195b2d1b5ab963826d0a169e377419742
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-06-27 00:53:13 -0600

    reverse integer without ternary operator

--- reverse-integer.js
+++ reverse-integer.js
@@ -1,6 +1,49 @@
 function reverseInteger(n) {
     let reversedNum = parseInt(String(n).split("").reverse().join(""));
+    // conditional ternary operator
+    // condition ? expression to execute if condition is truthy : expression to execute if falsy
     return n >= 0 ? reversedNum : reversedNum * -1;
 }
 
 console.log(reverseInteger(-250));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function reverseInt(n) {
+    let reversedInt = parseInt(String(n).split("").reverse().join(""));
+
+    if (n >= 0)
+        console.log(reversedInt)
+    else
+        console.log(reversedInt * -1)
+};
+
+reverseInt(-221)
