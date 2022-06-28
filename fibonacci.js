420f75871558f68745710d0ad3cdf3280e049416
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-06-27 18:37:12 -0600

    fibonacci algorithm added

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+// 0,1,1,2,3,5,8,13,21
+// what value is at the selected index?
+
+function fibonacci(n) {
+        return n < 1 ? 0
+                : n <= 2 ? 1
+                        : fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+console.log(fibonacci(3))
+// 2
