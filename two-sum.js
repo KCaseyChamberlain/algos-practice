e728f722db136c5c957b5662cf2d86962e572d0d
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-07-01 23:03:15 -0600

    two sum algorithim added

---
+++ two-sum.js
@@ -0,0 +1,14 @@
+let nums = [3, 2, 3]
+let target = 6
+
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++)
+        for (let j = i + 1; j < nums.length; j++)                                                                     if (nums[i] + nums[j] === target)
+            return [i, j]
+};
+
+console.log(
+    twoSum(nums, target)
+)
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
