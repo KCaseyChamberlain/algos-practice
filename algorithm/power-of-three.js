0ac178b04fbd47955fd9415e7f3b531b689ea68e
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-08-04 19:23:07 -0600

    git push origin main

---
+++ algorithm/power-of-three.js
@@ -0,0 +1,24 @@
+let n = 81
+// let n = 9
+
+var isPowerOfThree = function(n) {
+    if(n == 0){
+        return false
+    }
+    
+    while(n % 3 == 0){
+        n /= 3
+    }
+    
+    return n == 1
+};
+
+console.log(
+    isPowerOfThree(n)
+)
+
+// Given an integer n, return true if it is a power of three. 
+// Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x 
+// such that n == 3x.
