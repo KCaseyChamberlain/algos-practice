423af3b89fea2889466dc6bd43bcb34be7186039
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-08-14 22:19:39 -0600

    sql query added

---
+++ sql/rising-temperature.sql
@@ -0,0 +1,24 @@
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
+;
+
+-- Table: Weather
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | recordDate    | date    |
+-- | temperature   | int     |
+-- +---------------+---------+
+-- id is the primary key for this table.
+-- This table contains information about the temperature on a certain day.
+ 
+
+-- Write an SQL query to find all dates' Id with higher temperatures 
+-- compared to its previous dates (yesterday).
