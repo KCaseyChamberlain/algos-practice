6e1cef04c7e06aea279134f83ab0d77511e77a1c
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-09-26 21:59:58 -0600

    algorithim added

---
+++ algorithm/running-sum.js
@@ -0,0 +1,13 @@
+// Given an array nums. We define a running sum of an array as 
+// runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+let nums = [1, 2, 3, 4]
+
+var runningSum = function (nums) {
+    let sum = nums.map(res => {
+        return val = res + val
+    })
+    return sum
+};
