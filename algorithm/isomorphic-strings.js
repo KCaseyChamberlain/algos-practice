26d950d4e9b63185696d797bdf7350f46aee273f
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-09-28 22:44:58 -0600

    isomorphic string algorithm added

---
+++ algorithm/isomorphic-strings.js
@@ -0,0 +1,27 @@
+let s = "paper"
+let t = "title"
+
+var isIsomorphic = function (s, t) {
+    if (s.length !== t.length) return false;
+
+    const mapS = {};
+    const mapT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        let charS = s[i];
+        let charT = t[i];
+
+        if (!mapS[charS]) mapS[charS] = charT;
+        if (!mapT[charT]) mapT[charT] = charS;
+
+        if (mapS[charS] !== charT || mapT[charT] !== charS) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+console.log(
+    isIsomorphic(s,t)
+)
