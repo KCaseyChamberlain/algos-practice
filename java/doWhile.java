069366721b755fa5060a779ff236e1c544c36f03
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-10-12 22:08:36 -0600

    do while

---
+++ java/doWhile.java
@@ -0,0 +1,13 @@
+// do while will always run once, and continue to run if true.
+
+public class doWhile {
+   public static void main(String args[]) {
+      int x = 10;
+
+      do {
+         System.out.print("value of x : " + x);
+         x++;
+         System.out.print("\n");
+      } while (x < 20);
+   }
+}
