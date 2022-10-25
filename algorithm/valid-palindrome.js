5a26b03cf13011992b3318904da8cb792c3cc119
kcaseychamberlain <caseygchamberlain@gmail.com>
2022-10-24 21:17:15 -0600

    start of function

---
+++ algorithm/valid-palindrome.js
@@ -0,0 +1,20 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+let s = "A man, a plan, a canal: Panama"
+
+var isPalindrome = function (s) {
+    let hashMap = {}
+
+    for (ch of s) {
+        if (ch == ) {
+// turn uppercase lowercase
+// remove all non-alphanumeric characters
+        }
+    }
+    return s
+};
+
+console.log(
+    isPalindrome(s)
+)
